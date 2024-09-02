1a540342765aad2e27604b70b97aaa92fd979c8c
guimileib <guilhermemileib@gmail.com>
2024-09-02 09:49:17 -0300

    Atualização código ex02

--- LAB01 - Procedural/ex02/Main.java
+++ LAB01 - Procedural/ex02/Main.java
@@ -1,64 +1,42 @@
 import java.util.Scanner;
 
-// PS: Por algum motivo depois de conferir o codigo, não consegui rodar, mas pelo terminal consegui kkk
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         Produto[] produtos = new Produto[4];
+        Scanner s = new Scanner(System.in);
 
         for (int i = 0; i < 4; i++) {
-            System.out.println("\nInforme o nome, o preco e a qtd. em estoque do produto " + (i + 1) + ":");
-            String nome = scanner.next();
-            float preco = scanner.nextFloat();
-            int qtdEstoque = scanner.nextInt();
-
-            produtos[i] = new Produto(nome, preco, qtdEstoque);
+            System.out.println("\nInforme o nome, o preço e a qtd. em estoque do produto " + (i + 1) + ":");
+            String nome = s.next();
+            float preco = s.nextFloat();
+            int qtdEstoque = s.nextInt();
+        
+            produtos[i] = new Produto(nome, preco, qtdEstoque);                                         
         }
-
-        System.out.println("\nAumentando o preco em 10% do produto 1 e 3");
+        System.out.println("\nAumentando o preço em 10% do produto 1 e 3");
 
         produtos[0].alteraPreco(10);
         produtos[2].alteraPreco(10);
 
-        System.out.println("\nReduzindo o preco em 5% do produto 2");
+        System.out.println("\nReduzindo o preço em 5% do produto 2");
 
         if (produtos[1].alteraPreco(-5) == -1) {
-            System.out.println("\nErro: preco nao alterado. Porcentagem invalida");
+            System.out.println("\nErro: preço não alterado. Porcentagem inválida");
         } else {
-            System.out.println("\nPreco alterado com sucesso");
+            System.out.println("\nPreço alterado com sucesso");
         }
 
-        System.out.println("\nAlterando o preco do produto 4");
+        System.out.println("\nAlterando o preço do produto 4");
         if (produtos[3].alteraPreco(-110) == -1) {
-            System.out.println("\nErro: preco nao alterado. Porcentagem invalida");
+            System.out.println("\nErro: preço não alterado. Porcentagem inválida");
         }
 
         System.out.println("\nProdutos Cadastrados:");
         for (Produto produto : produtos) {
-            System.out.printf("\nProduto: %s\nPreco: %.2f\nEstoque: %d\n", produto.nome, produto.preco, produto.qtdEstoque);
+            System.out.printf("\nProduto: %s\nPreço: %.2f\nEstoque: %d\n", produto.nome, produto.preco, produto.qtdEstoque);
         }
     }
 }
 
-class Produto {
-    String nome;
-    float preco;
-    int qtdEstoque;
 
-    Produto(String nome, float preco, int qtdEstoque) {
-        this.nome = nome;
-        this.preco = preco;
-        this.qtdEstoque = qtdEstoque;
-    }
 
-    int alteraPreco(float porcentagem) {
-        if (porcentagem > -100) {
-            float temp = 1 + porcentagem / 100; // Ex: 10 => 1 + 10/100 = 1.1; -5 => 1 + (-5/100) = 0.95
-            preco *= temp;
-            return 0;
-        } else {
-            return -1;
-        }
-    }
-}
