e844dfbfcfa53c7443d3a39eb1541aacc4818328
guimileib <guilhermemileib@gmail.com>
2024-10-09 17:42:17 -0300

    lista3 ex01 feito

---
+++ LAB03 - Class Relations/ex01/src/Locacao.java
@@ -0,0 +1,27 @@
+public class Locacao {
+    private int numero;
+    private String dataRetirada;
+    private String dataDevolucao;
+
+    
+    public int getNumero() {
+        return numero;
+    }
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    public String getDataRetirada() {
+        return dataRetirada;
+    }
+    public void setDataRetirada(String dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+    public String getDataDevolucao() {
+        return dataDevolucao;
+    }
+    public void setDataDevolucao(String dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+
+}
